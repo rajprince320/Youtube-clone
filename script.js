4b5e24ceef2f10e2db37e3a03804ca44fa9cb20e
rajprince320 <rajprince320@gmail.com>
2023-08-03 22:05:54 +0530

    search bar updated

---
+++ script.js
@@ -0,0 +1,91 @@
+var apiKey = "AIzaSyAeMc_WnQq3Rl4nFViAsaJs3xxWkakdSa8";
+
+// let url = `https://youtube.www.googleapis.com/youtube/v3/search?part=snippet&macResults=100&chart=mostPopular&regionCode=IN&key=${apiKey}`;
+
+let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=${apiKey}`;
+
+async function mostPopular(url) {
+  let link = await fetch(url).catch((err) => console.log(err));
+  let data = await link.json();
+  console.log(data.items.value);
+  appendData(data.items);
+}
+mostPopular(url);
+
+function appendData(data) {
+  let container = document.getElementById("container");
+  container.innerHTML = null;
+  data.forEach(({ snippet, id: videoId }) => {
+    let img = snippet.thumbnails.high.url;
+    let title = snippet.title;
+    let cTitle = snippet.channelTitle;
+    let icon = snippet.thumbnails.default.url;
+
+    let div = document.createElement("div");
+    div.className = "videoCard";
+    let image = document.createElement("img");
+    image.src = img;
+    let card_data = document.createElement("div");
+    card_data.className = "card-data";
+
+    let CIcon = document.createElement("img");
+    CIcon.src = icon;
+    CIcon.className = "channel-icon";
+    let ChannelIcon = document.createElement("div");
+    ChannelIcon.className = "channel-icon";
+    let CTitle = document.createElement("div");
+    CTitle.className = "title";
+    let h3 = (document.createElement("h3").innerText = title);
+    let CTtile = (document.createElement("p").innerText = cTitle);
+
+    ChannelIcon.append(CIcon);
+    CTitle.append(h3, CTtile);
+    card_data.append(ChannelIcon, CTitle);
+
+    div.append(image, card_data);
+    container.append(div);
+
+    let data = {
+      snippet,
+      videoId,
+    };
+
+    div.addEventListener("click", function () {
+      localStorage.setItem("video", JSON.stringify(data));
+      window.location.href = "./videoDetails.html";
+    });
+
+    // image.src = img;
+    // let name = document.createElement("p");
+    // name.innerText = title;
+    // let Channel = document.createElement("p");
+    // Channel.innerText = cTitle;
+    // div.append(image, CTitle, h3);
+    // container.append(div);
+
+    // container.innerHTML += `
+    // <div id="videoCard" class="videoCard" onclick="${video()}">
+    //   <img src="${img}"/>
+    //   <div class="card-data">
+    //     <div class="channel-icon">
+    //       <img src="${icon}" class="channel-icon" alt="">
+    //     </div>
+    //     <div class="title">
+    //       <h3>${title}</h3>
+    //       <p>${cTitle}</p>
+    //     </div>
+    //   </div>
+    // </div>`;
+  });
+}
+
+async function search() {
+  let query = document.getElementById("query").value;
+  let res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=${apiKey}`
+    // `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=${apiKey}`
+  ).catch((err) => console.log(err));
+  // console.log(query);
+  let data = await res.json();
+  appendData(data.items);
+}
---
+++ videoDetails.js
@@ -0,0 +1,26 @@
+let data = JSON.parse(localStorage.getItem("video"));
+function playVideo(data) {
+  let container = document.getElementById("play");
+  let iframe = document.createElement("iframe");
+  iframe.src = `https://www.youtube.com/embed/${data.videoId}?autoplay=1`;
+  iframe.width = "100%";
+  iframe.height = "413px";
+  iframe.setAttribute = ("allowfullscreen", true);
+  container.append(iframe);
+}
+
+playVideo(data);
+
+console.log(data);
+
+{
+  /* <iframe
+  width="735"
+  height="413"
+  src="https://www.youtube.com/embed/wr2DiLeDSVI"
+  title="YouTube Data API v3 Tutorial | OAuth | Youtube Api OAuth | Get Youtube Channel List Api | Hindi | #1"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  allowfullscreen
+></iframe>; */
+}
